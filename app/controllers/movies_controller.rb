ff02c7688ecdc9ba47c3a08d8546756db7dcd4a1
Mike Adeleke <madeleke13@gmail.com>
2014-01-22 19:39:05 -0600

    finished module 19, the flash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,9 +14,9 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie successfully updated!"
     else
-      render :new
+      render :edit
     end
   end
 
@@ -27,7 +27,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie
+      redirect_to @movie, notice: "Movie was successfully created!"
     else
       render :new
     end
@@ -36,7 +36,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url
+    redirect_to movies_url, alert: "Movie successfully deleted!"
   end
   
 private
